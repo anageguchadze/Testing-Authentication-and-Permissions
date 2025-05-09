534e69b80cc06f7e2cca85b78bfc8e5b484b0b3d
Ana <anageguchadze22@gmail.com>
2025-05-09 17:26:31 +0400

    Add model, serializer, viewset, urls

--- testing_app/views.py
+++ testing_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import PrivateItem
+from .serializers import PrivateItemSerializer
 
-# Create your views here.
+class PrivateItemViewSet(viewsets.ModelViewSet):
+    queryset = PrivateItem.objects.all()
+    serializer_class = PrivateItemSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
